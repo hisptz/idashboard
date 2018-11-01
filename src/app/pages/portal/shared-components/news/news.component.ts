7f686ed1ef07012de37fb6785aae8a06f6cff878
josephatj <josephatjulius24@gmail.com>
2018-11-01 20:37:31 +0300

    add news component

---
+++ src/app/pages/portal/shared-components/news/news.component.ts
@@ -0,0 +1,54 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.css']
+})
+export class NewsComponent implements OnInit {
+
+  @Input() allNews: any;
+  constructor() { }
+
+  ngOnInit() {
+    $.fn.liScroll = function(settings) {
+      settings = $.extend({
+        travelocity: 0.03
+      }, settings);
+      return this.each(function() {
+        const $strip = $(this);
+        $strip.addClass('newsticker')
+        let stripHeight = 1;
+        $strip.find('li').each(function(i) {
+          stripHeight += $(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
+        });
+        const $mask = $strip.wrap('<div class=' + '"mask"' + '></div>');
+        const $tickercontainer = $strip.parent().wrap('<div class=' + '"tickercontainer"' + '></div>');
+        const containerHeight = $strip.parent().parent().height();
+        $strip.height(stripHeight);
+        const totalTravel = stripHeight;
+        const defTiming = totalTravel / settings.travelocity;
+        function scrollnews(spazio, tempo) {
+          $strip.animate({top: '-=' + spazio}, tempo, 'linear', function() {$strip.css('top', containerHeight); scrollnews(totalTravel, defTiming);});
+        }
+        scrollnews(totalTravel, defTiming);
+        $strip.hover(function() {
+            $(this).stop();
+          },
+          function() {
+            const offset = $(this).offset();
+            const residualSpace = offset.top + stripHeight;
+            const residualTime = residualSpace / settings.travelocity;
+            scrollnews(residualSpace, residualTime);
+          });
+      });
+    };
+
+    $(function() {
+      $('ul#scrollingUl').liScroll();
+    });
+  }
+
+}
