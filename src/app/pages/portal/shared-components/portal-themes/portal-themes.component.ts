b1fc0a7cd2e2a28655b0eb7667d8f396a2e2798f
josephatj <josephatjulius24@gmail.com>
2018-11-01 11:46:03 +0300

    add themes component

--- src/app/pages/portal/shared-components/portal-themes/portal-themes.component.ts
+++ src/app/pages/portal/shared-components/portal-themes/portal-themes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-portal-themes',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortalThemesComponent implements OnInit {
 
+  @Input() themes: any;
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/portal/shared-components/statistics-summary/statistics-summary.component.ts
+++ src/app/pages/portal/shared-components/statistics-summary/statistics-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-statistics-summary',
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsSummaryComponent implements OnInit {
 
-  constructor() { }
+  @Input() statsSummaryGroups: Array<any>;
+  constructor() {
+  }
 
   ngOnInit() {
   }
