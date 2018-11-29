0e1cd0ddab4d27f6a28b4b990a749d62ac49dfc5
Joseph Chingalo <profschingalo@gmail.com>
2018-11-29 11:33:11 +0300

    fix(Printing): Fixing width of chart on printing

--- src/chart-resize.js
+++ src/chart-resize.js
@@ -1,15 +1,16 @@
-window.onbeforeprint = function() {
-  $(Highcharts.charts).each(function(i, chart) {
+window.onbeforeprint = function () {
+  $(Highcharts.charts).each(function (i, chart) {
     chart.oldhasUserSize = chart.hasUserSize;
     chart.resetParams = [chart.chartWidth, chart.chartHeight, false];
 
     var height = chart.renderTo.clientHeight;
     var width = chart.renderTo.clientWidth;
+    width -= 230;
     chart.setSize(width, height, false);
   });
 };
-window.onafterprint = function() {
-  $(Highcharts.charts).each(function(i, chart) {
+window.onafterprint = function () {
+  $(Highcharts.charts).each(function (i, chart) {
     chart.setSize.apply(chart, chart.resetParams);
     chart.hasUserSize = chart.oldhasUserSize;
   });
