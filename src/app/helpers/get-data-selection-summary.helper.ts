be56f961e82fc9ffe1d2db1f9154b53e644719c9
Rajab Mkomwa <rajeyw@gmail.com>
2019-01-10 21:54:11 +0300

    Fixed issues on global summary

--- src/app/helpers/get-data-selection-summary.helper.ts
+++ src/app/helpers/get-data-selection-summary.helper.ts
@@ -5,22 +5,32 @@ export function getDataSelectionSummary(
   dataSelections: VisualizationDataSelection[]
 ) {
   const ouDimension = _.find(dataSelections, ['dimension', 'ou']);
-  const ouSection = (
-    _.head(ouDimension ? ouDimension.items : []) || { name: '' }
-  ).name;
+
+  const ouItems: any[] = ouDimension ? ouDimension.items : [];
+
+  const ouSection = _.join(
+    _.take(_.map(ouItems, (item: any) => item.name), 3),
+    ', '
+  );
+
+  const moreOuSection =
+    ouItems.length > 3 ? ` and ${ouItems.length - 3} more ` : '';
 
   const peDimension = _.find(dataSelections, ['dimension', 'pe']);
+  const peItems: any[] = peDimension ? peDimension.items : [];
   const peSection = _.uniq(
     _.filter(
       [
-        (_.head(peDimension ? peDimension.items : []) || { name: '' }).name,
-        (_.last(peDimension ? peDimension.items : []) || { name: '' }).name
+        (_.head(peItems) || { name: '' }).name,
+        (_.last(peItems) || { name: '' }).name
       ],
       peName => peName !== undefined || peName !== ''
     )
   ).join(' - ');
 
   return ouSection !== '' && peSection !== ''
-    ? ouSection + ' - ' + peSection
+    ? `Showing data from <b>${ouSection}${moreOuSection}</b> in the period${
+        peItems.length > 1 ? 's' : ''
+      } ${peItems.length > 1 ? 'between' : 'of'} <b>${peSection}</b>`
     : '';
 }
