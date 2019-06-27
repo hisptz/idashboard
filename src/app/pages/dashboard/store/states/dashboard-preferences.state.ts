abd5dd54701e091957817bc848a29d9aa3ff58cb
Rajab Mkomwa <rajeyw@gmail.com>
2019-06-27 18:26:51 +0300

    chore(package.json): update @iapp/ngx-dhis2-http-client to 2.0.7

--- src/app/pages/dashboard/store/states/dashboard-preferences.state.ts
+++ src/app/pages/dashboard/store/states/dashboard-preferences.state.ts
@@ -2,10 +2,10 @@ import { BaseState, initialBaseState } from 'src/app/store/states/base.state';
 import { DashboardPreferences } from '../../models/dashboard-preferences.model';
 
 export interface DashboardPreferencesState extends BaseState {
-  systemInfo: DashboardPreferences;
+  dashboardPreferences: DashboardPreferences;
 }
 
 export const initialDashboardPreferencesState: DashboardPreferencesState = {
   ...initialBaseState,
-  systemInfo: null
+  dashboardPreferences: null
 };
