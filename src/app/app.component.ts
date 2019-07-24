7998856db9db8b91ab3c567446863680c54b0c04
Rajab Mkomwa <rajeyw@gmail.com>
2019-07-24 23:40:58 +0300

    feat: add support to deduce application title from manifest file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Title } from '@angular/platform-browser';
+import {
+  NgxDhis2HttpClientService,
+  Manifest
+} from '@iapps/ngx-dhis2-http-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private translate: TranslateService,
+    private http: NgxDhis2HttpClientService,
     private titleService: Title
   ) {
     // this language will be used as a fallback when a translation isn't found in the current language
@@ -19,7 +24,18 @@ export class AppComponent {
     this.translate.use('en');
 
     // Set application title
-    this.setTitle('Interative Dashboard');
+    this.setTitle('Loading Dashboard...');
+  }
+
+  ngOnInit() {
+    // TODO Resolve manifest method from http service to avoid duplicate calls
+    this.http
+      .get('manifest.webapp', { isExternalLink: true })
+      .subscribe((manifest: Manifest) => {
+        if (manifest) {
+          this.setTitle(manifest.name);
+        }
+      });
   }
 
   public setTitle(newTitle: string) {
